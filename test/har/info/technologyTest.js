a1a01482cde0eaa3c98f222abbbb89bf08a44a2e
Peter Hedenskog <peter@soulgalore.com>
2020-11-13 21:18:58 +0100

    Use Wappalyzer to identify software. (#28)
    * Use Wappalyzer to identify software.

---
+++ test/har/info/technologyTest.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let assert = require('assert');
+let har = require('../../help/har');
+
+describe('Check for technology', function() {
+  it('We shave the right amount', function() {
+    return har.firstAdviceForTestFile('withHtmlContent.har').then(result => {
+      assert.strictEqual(
+        result.info.technology.length,
+        5
+      );
+    });
+  });
+
+});
