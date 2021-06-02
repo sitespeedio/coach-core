61f7eedcad183a9c1448f30cb60cf7403c000e43
soulgalore <peter@soulgalore.com>
2021-06-02 14:08:50 +0200

    Better test output

--- test/har/info/technologyTest.js
+++ test/har/info/technologyTest.js
@@ -4,7 +4,7 @@ let assert = require('assert');
 let har = require('../../help/har');
 
 describe('Check for technology', function() {
-  it('We shave the right amount', function() {
+  it('We shave the right amount of technologies', function() {
     return har.firstAdviceForTestFile('withHtmlContent.har').then(result => {
       assert.strictEqual(
         result.info.technology.length,
