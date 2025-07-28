c3a2cd98a31cd404c742b075191d5878dd507d00
Peter Hedenskog <peter@soulgalore.com>
2025-07-28 20:52:58 +0200

    A smarter test server setup (#127)

--- test/help/webserver.js
+++ test/help/webserver.js
@@ -1,27 +1,31 @@
 'use strict';
 
-let Promise = require('bluebird'),
-  fs = require('fs'),
-  connect = require('connect'),
-  serveStatic = require('serve-static'),
-  http = require('http'),
-  http2 = require('http2'),
-  path = require('path');
+const Promise = require('bluebird');
+const fs = require('fs');
+const connect = require('connect');
+const serveStatic = require('serve-static');
+const http = require('http');
+const http2 = require('http2');
+const path = require('path');
 
 Promise.promisifyAll(fs);
 
+let server;
+let isListening = false;
+
 function createServer(useHttp2) {
-  const testdataFolder = path.join(__dirname, '..'),
-    app = connect();
+  const testdataFolder = path.join(__dirname, '..');
+  const app = connect();
 
   app.use(serveStatic(path.resolve(testdataFolder, 'http-server'), {}));
+
   if (useHttp2) {
-    const certsFolder = path.join(testdataFolder, 'testdata', 'certs'),
-      httpsOptions = {
-        key: fs.readFileSync(path.join(certsFolder, 'server.key'), 'utf8'),
-        cert: fs.readFileSync(path.join(certsFolder, 'server.crt'), 'utf8'),
-        passphrase: 'coach'
-      };
+    const certsFolder = path.join(testdataFolder, 'testdata', 'certs');
+    const httpsOptions = {
+      key: fs.readFileSync(path.join(certsFolder, 'server.key'), 'utf8'),
+      cert: fs.readFileSync(path.join(certsFolder, 'server.crt'), 'utf8'),
+      passphrase: 'coach'
+    };
 
     return http2.createServer(httpsOptions, app);
   } else {
@@ -29,23 +33,32 @@ function createServer(useHttp2) {
   }
 }
 
-let server;
-
 module.exports = {
   async startServer(useHttp2) {
     if (!server) {
       server = createServer(useHttp2);
     }
-    return new Promise((resolve, reject) => {
-      server
-        .listen(0, '0.0.0.0')
-        .on('error', e => reject(e))
-        .on('listening', () => resolve(server.address()));
-    });
+
+    if (!isListening) {
+      await new Promise((resolve, reject) => {
+        server
+          .listen(0, '0.0.0.0')
+          .on('error', reject)
+          .on('listening', () => {
+            isListening = true;
+            resolve(server.address());
+          });
+      });
+    }
+
+    return server.address();
   },
+
   async stopServer() {
-    return Promise.resolve(server.close()).finally(() => {
+    if (server && isListening) {
+      await Promise.resolve(server.close());
       server = undefined;
-    });
+      isListening = false;
+    }
   }
 };
